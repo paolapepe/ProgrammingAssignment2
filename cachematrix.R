161a06895458d71d9e431518faa49b331c51535a
Paola Pepe <paolaperezpena89@gmail.com>
2020-07-29 22:14:33 -0500

    Homework_week_3.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## A function that received a special "matrix" object and cache the inverse
+## of the matrix and store in the global environment
+
 
 makeCacheMatrix <- function(x = matrix()) {
+  ##initialize the inverse matrix  
+  inv <- NULL
+  #set function for the matrix
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  ##define the return matrix object by defining it as get
+  get <- function() {
+    x
+  }
+  ##setting the inverse matrix 
+  setInverse <- function(inverse) {
+    
+    ##assign the inverse matrix to the environment var
+     inv <<- inverse
+  }
+    
+  ##define the function for returning the inverse matrix  
+  getInverse <- function() {
+    inv
+    
+  }
+  ##put all defined function into a list for future call within the environment
+  list (set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      
+  inverse <- x$getInverse()
+  
+  if (!is.null(inverse)){
+    if ( identical(x$get() %*% inverse, inverse %*% x$get())) {
+      print ("Getting cached data")
+      return(inverse)
+    }
+    
+    
+  }
+  
+  #Else inverse matrix null or matrix change, calculates the inverse 
+  data <- x$get()
+  inverse <- solve(data,...)
+  x$setInverse(inverse)
+  
+  inverse
+    
 }
